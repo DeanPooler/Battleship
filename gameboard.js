42dee0d2c008338ccf6bb57113be357a04907f60
Dean Pooler <Deanpooler@outlook.com>
2023-04-17 13:44:12 +0200

    Add gameboard factory function and tests

--- gameboard.js
+++ gameboard.js
@@ -3,12 +3,16 @@ const Ship = require("./ship");
 const Gameboard = () => {
     let board = new Array(10).fill(0).map(() => new Array(10).fill(0));
     let moveLog = [];
+    let shipCount = 0;
+    let sunkCount = 0;
 
     const getBoard = () => { return board; }
     const getMoveLog = () => { return moveLog; }
+    const allShipsSunk = () => { return shipCount === sunkCount ? true : false };
 
     const placeShip = (col, row, size, or) => {
         const ship = Ship(size);
+        shipCount += 1;
         for (let i = 0; i < size; i++) {
             if (or === 'vertical') { board[col + i][row] = ship; }
             else if (or === 'horizontal') { board[col][row + i] = ship; }
@@ -24,6 +28,7 @@ const Gameboard = () => {
         } else if (board[col][row] !== 0) {
           board[col][row].hit();
           if (board[col][row].isSunk()) {
+            sunkCount += 1;
             return `Sunk ship at column ${col}, row ${row}`;
           } else {
             return `Hit ship at column ${col}, row ${row}`;
@@ -31,7 +36,7 @@ const Gameboard = () => {
         }
     }
 
-    return { getBoard, getMoveLog, placeShip, receiveAttack };
+    return { getBoard, getMoveLog, placeShip, receiveAttack, allShipsSunk };
 }
 
 module.exports = Gameboard
